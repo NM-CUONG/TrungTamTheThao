8c0c0f2f324a265a0ae8e6fa470dd0c8da9dcbe7
cuongnm2002 <nguyenmanhcuong02062002@gmail.com>
2024-04-04 23:01:45 +0700

    Bước đầu tạo emailconfrim và mã hóa mật khẩu

---
+++ TrungTamTheThao/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public class PasswordManager
+        {
+            // Mã hóa mật khẩu bằng bcrypt
+            public static string HashPassword(string password)
+            {
+                return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+            }
+
+            // Kiểm tra mật khẩu đã nhập với mật khẩu đã mã hóa bằng bcrypt
+            public static bool VerifyPassword(string enteredPassword, string hashedPassword)
+            {
+                return BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
+            }
+        }
+    }
+}
--- TrungTamTheThao/WebApp/Controllers/HomeController.cs
+++ TrungTamTheThao/WebApp/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private DBContext db = new DBContext();
         public ActionResult Index()
         {
             return View();
@@ -31,41 +32,81 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public ActionResult Login() {
+        public ActionResult Regis() 
+        {
             return View ();
         }
 
-        public ActionResult SendMail()
+        public ActionResult SendMail(tb_User model)
         {
+            //Sinh userid tự động
+            var lastItem = db.tb_User.OrderByDescending(x => x.ID).FirstOrDefault();
+            model.UserID = "U" + lastItem.ID;
+            // Mã hóa mật khẩu trước khi lưu
+            model.Password = AccountController.PasswordManager.HashPassword(model.Password);
+            //Lưu
+            db.tb_User.Add(model);
+            db.SaveChanges();
             try
             {
-                using (MailMessage mm = new MailMessage("nguyenmanhcuong2k2.hsbg@gmail.com", "nguyenmanhcuong02062002@gmail.com"))
+                using (MailMessage mm = new MailMessage("nguyenmanhcuong2k2.hsbg@gmail.com", model.Email))
                 {
-                    mm.Subject = "Test Mail";
-                    mm.Body = "Xin chao";
-                    mm.IsBodyHtml = false;
+                    //Tiêu đề mail
+                    mm.Subject = "Xác nhận đăng ký tài khoản";
+                    //Nội dung mail - gửi kèm link xác nhận có userid để nhận diện khi confirm
+                    mm.Body = $"Để xác nhận tài khoản vui lòng nhấp vào link sau <a href=\"https://localhost:44315/Home/ConfirmEmailRegis/?userid={@model.UserID}\">Xác nhận đăng ký tài khoản</a>";
+                    mm.IsBodyHtml = true;
                     using (SmtpClient smtp = new SmtpClient())
                     {
                         smtp.Host = "smtp.gmail.com";
                         smtp.EnableSsl = true;
 
+                        //tài khoản đăng kí sử dụng smtp
                         NetworkCredential cred = new NetworkCredential("nguyenmanhcuong2k2.hsbg@gmail.com", "amyr nbnb akdc djgc");
                         smtp.UseDefaultCredentials = true;
                         smtp.Credentials = cred;
                         smtp.Port = 587;
                         smtp.Send(mm);
-                        ViewBag.Message = "Da gui mail";
                     }
                 }
             }
             catch (Exception)
             {
-                throw;
+                ViewBag.Msg = "Đã có lỗi trong quá trình gửi!";
             }
-            
 
+            ViewBag.Msg = "Đã gửi mail";
+            return View("Regis");
+        }
+
+        public ActionResult ConfirmEmailRegis(string userid)
+        {
+            //lay ra account vua dang ky de xac nhan bang cach chinh status = 1
+            var account = db.tb_User.Where(x => x.UserID == userid).FirstOrDefault();
+            if (account != null)
+            {
+                account.Status = 1;
+                db.SaveChanges();
+            }
             return View();
         }
-        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tb_User model)
+        {
+            var account = db.tb_User.Where(x => x.UserName == model.UserName).FirstOrDefault();
+            if (account != null)
+            {
+                if (AccountController.PasswordManager.VerifyPassword(model.Password, account.Password))
+                {
+                    return Redirect("/Home/Index");
+                }
+            }
+            return View();
+        }
+
     }
 }
