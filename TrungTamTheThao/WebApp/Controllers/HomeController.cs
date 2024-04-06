cfa70014979149d8493fed74ed6fa2a62fa5b7c9
cuongnm2002 <nguyenmanhcuong02062002@gmail.com>
2024-04-07 00:53:02 +0700

    dang xu ly doan bat thong bao khi dang ky

--- TrungTamTheThao/WebApp/Controllers/HomeController.cs
+++ TrungTamTheThao/WebApp/Controllers/HomeController.cs
@@ -10,59 +10,95 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
+
+    //Mã hóa mật khẩu
+    public class PasswordManager
+    {
+        // Mã hóa mật khẩu bằng bcrypt
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+        }
+
+        // Kiểm tra mật khẩu đã nhập với mật khẩu đã mã hóa bằng bcrypt
+        public static bool VerifyPassword(string enteredPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
+        }
+    }
+    //Các chức năng
     public class HomeController : Controller
     {
-        private DBContext db = new DBContext();
+        //đối tượng ánh xạ CSDL
+        private readonly DBContext db = new DBContext();
+
+        //Cấu hình email
+        private static string EmailHost = "smtp.gmail.com";
+        private static string EmailPort= "587";
+        private static string EmailFrom= "nguyenmanhcuong2k2.hsbg@gmail.com";
+        private static string EmailFromPassword= "amyr nbnb akdc djgc";
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        //Check trùng UserName
+        public bool CheckExistsUserName(string UserName)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return db.tb_User.Any( x => x.UserName == UserName);
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
+        //Đăng ký
         public ActionResult Regis() 
         {
             return View ();
         }
 
-        public ActionResult SendMail(tb_User model)
+        [HttpPost]
+        public JsonResult Regis(tb_User model)
         {
+
+            //Check trùng username
+            if (CheckExistsUserName(model.UserName))
+            {
+                return Json(new { success = false, message = "Tên đăng nhập đã tồn tại"});
+            }
             //Sinh userid tự động
             var lastItem = db.tb_User.OrderByDescending(x => x.ID).FirstOrDefault();
             model.UserID = "U" + lastItem.ID;
             // Mã hóa mật khẩu trước khi lưu
-            model.Password = AccountController.PasswordManager.HashPassword(model.Password);
+            model.Password = PasswordManager.HashPassword(model.Password);
             //Lưu
             db.tb_User.Add(model);
             db.SaveChanges();
+
+            if(!SendMail(model.Email, model.UserID))
+            {
+                return Json(new { success = false, message = "Không thể gửi email xác nhận đăng ký" });
+            }
+
+            return Json(new { success = true, message = "Vui lòng truy cập email để hoàn tất đăng ký tài khoản" });
+        }
+
+        //Gửi Email
+        public bool SendMail(string EmailTo, string UserID)
+        {
             try
             {
-                using (MailMessage mm = new MailMessage("nguyenmanhcuong2k2.hsbg@gmail.com", model.Email))
+                using (MailMessage mm = new MailMessage(EmailFrom, EmailTo))
                 {
                     //Tiêu đề mail
                     mm.Subject = "Xác nhận đăng ký tài khoản";
                     //Nội dung mail - gửi kèm link xác nhận có userid để nhận diện khi confirm
-                    mm.Body = $"Để xác nhận tài khoản vui lòng nhấp vào link sau <a href=\"https://localhost:44315/Home/ConfirmEmailRegis/?userid={@model.UserID}\">Xác nhận đăng ký tài khoản</a>";
+                    mm.Body = $"Để xác nhận tài khoản vui lòng nhấp vào link sau <a href=\"https://localhost:44315/Home/ConfirmEmailRegis/?userid={UserID}\">Xác nhận đăng ký tài khoản</a>";
                     mm.IsBodyHtml = true;
                     using (SmtpClient smtp = new SmtpClient())
                     {
-                        smtp.Host = "smtp.gmail.com";
+                        smtp.Host = EmailHost;
                         smtp.EnableSsl = true;
 
                         //tài khoản đăng kí sử dụng smtp
-                        NetworkCredential cred = new NetworkCredential("nguyenmanhcuong2k2.hsbg@gmail.com", "amyr nbnb akdc djgc");
+                        NetworkCredential cred = new NetworkCredential(EmailFrom, EmailFromPassword);
                         smtp.UseDefaultCredentials = true;
                         smtp.Credentials = cred;
                         smtp.Port = 587;
@@ -72,13 +108,13 @@ namespace WebApp.Controllers
             }
             catch (Exception)
             {
-                ViewBag.Msg = "Đã có lỗi trong quá trình gửi!";
+                return false;
             }
 
-            ViewBag.Msg = "Đã gửi mail";
-            return View("Regis");
+            return true;
         }
 
+        //Xác nhận email đăng ký tài khoản
         public ActionResult ConfirmEmailRegis(string userid)
         {
             //lay ra account vua dang ky de xac nhan bang cach chinh status = 1
@@ -90,17 +126,22 @@ namespace WebApp.Controllers
             }
             return View();
         }
+
+        //Hiển thị màn hình đăng nhập
         public ActionResult Login()
         {
             return View();
         }
+
+        // Xử lý đăng nhập
         [HttpPost]
         public ActionResult Login(tb_User model)
         {
             var account = db.tb_User.Where(x => x.UserName == model.UserName).FirstOrDefault();
             if (account != null)
             {
-                if (AccountController.PasswordManager.VerifyPassword(model.Password, account.Password))
+                //Giải mã mật khẩu
+                if (PasswordManager.VerifyPassword(model.Password, account.Password))
                 {
                     return Redirect("/Home/Index");
                 }
@@ -109,4 +150,5 @@ namespace WebApp.Controllers
         }
 
     }
+
 }
